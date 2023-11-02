5c19f05deb6ff00c0926a2d5b76b3365534458e6
nblancoz <nblancozappala@gmail.com>
2023-11-02 09:24:13 +0100

    Endpoint to  update a product added

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,32 @@
-const { Product } = require("../models/index.js")
+const { Product } = require("../models/index.js");
 
 const ProductController = {
-    create(req, res) {
-        Product.create(req.body)
-        .then(product => res.status(201).send({message: "Product created successfully", product}))
-        .catch(err => console.error(err))
+  create(req, res) {
+    Product.create(req.body)
+      .then((product) =>
+        res
+          .status(201)
+          .send({ message: "Product created successfully", product })
+      )
+      .catch((err) => console.error(err));
+  },
+  async update(req, res) {
+    try {
+      await Product.update(req.body, {
+        where: {
+          id: req.params.id
+        },
+      });
+      res.send({ message: "Product updated successfully" });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send({
+          message: "Unexpected error while trying to update de product",
+        });
     }
-}
+  },
+};
 
-module.exports = ProductController
+module.exports = ProductController;
--- routes/products.js
+++ routes/products.js
@@ -4,5 +4,6 @@ const ProductController = require("../controllers/ProductController");
 const { authentication, isAdmin } = require("../middleware/authentication");
 
 router.post("/create", authentication, isAdmin, ProductController.create);
+router.put("/updateById/:id", authentication, isAdmin, ProductController.update)
 
 module.exports = router;
