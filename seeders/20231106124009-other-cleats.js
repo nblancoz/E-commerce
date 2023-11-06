c324bc367b215f50128f2fd60e1b9fd765e0a20e
nblancoz <nblancozappala@gmail.com>
2023-11-06 13:47:33 +0100

    added seeders

---
+++ seeders/20231106124009-other-cleats.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Products", [
+      {
+        name: "Second cleats",
+        description: "Second cleats created with seeders",
+        price: 160,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
---
+++ seeders/20231106124104-soccer-socks.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Products", [
+      {
+        name: "Soccer socks",
+        description: "Soccer socks created with seeders",
+        price: 10,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
